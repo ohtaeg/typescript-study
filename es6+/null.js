9e99a87be6d062abc02b508995890f1dc6d5d5c1
ohtaeg <otk1090@naver.com>
2023-11-29 01:58:28 +0900

    주석 추가

--- es6+/null.js
+++ es6+/null.js
@@ -5,7 +5,7 @@ const ob = {
 }
 console.log(ob.asdf); // 없는 키값이라 undefined가 나옴
 
-// ? 는 값이 있는지 없는지 모르겠다라는 뜻으로 참조하는 변수에 ?를 붙여 이 값이 없으면 undefined를 출력하세요를 의미
+// ? 는 자바의 Optional과 비슷한 느낌으로 값이 있는지 없는지 모르겠다라는 뜻으로 참조하는 변수에 ?를 붙여 이 값이 없으면 undefined를 출력하세요를 의미
 // 해당 참조 변수에 값이 있을 수도 없을수도 있는 상황에 씀
 console.log(ob?.asdf?.hi);
 
