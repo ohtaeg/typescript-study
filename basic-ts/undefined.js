cfe5bb601f5df8a505bacc9e25f1d283401ec6cf
ohtaeg <otk1090@naver.com>
2023-11-29 00:51:29 +0900

    undefined 정리

---
+++ basic-ts/undefined.js
@@ -0,0 +1,8 @@
+"use strict";
+// undefined
+// 왼쪽은 타입, 오른쪽은 값
+const un = undefined;
+function printName(params) {
+    console.log(params.name);
+}
+printName({ name: "ohtaeg" });
---
+++ basic-ts/undefined.ts
@@ -0,0 +1,18 @@
+// undefined
+
+// 왼쪽은 타입, 오른쪽은 값
+const un: undefined = undefined;
+
+// 모든 타입의 최하위 계층인 undefined
+interface Man {
+    name : string;
+}
+
+function printName(params: Man) {
+    console.log(params.name);
+}
+// tsconfig.json > compilerOptions 에서 strictNullChecks 옵션을 false로 하면 런타임에서 undefined 참조 에러 발생하게 된다.
+// 모든 타입의 최하위 계층인 undefined이다 보니 값이 정상적으로 들어가는 것으로 인지하기 때문에 문제!!
+// 그래서 ? or ?? 연산자를 잘 활용해주자
+// printName(undefined);
+
