049dba151d77276a6c6483e696eb11d89bd8a0f7
ohtaeg <otk1090@naver.com>
2023-11-30 23:04:05 +0900

    주석  추가

--- intermediate-ts/generic/examples.js
+++ intermediate-ts/generic/examples.js
@@ -1,6 +1,26 @@
 "use strict";
-// 파라미터의 키를 반환해주는 함수를 만들고자 한다.
-// 
+/**
+ * @param params
+ * @param key
+ * 파라미터의 키에 해당하는 값을 반환해주는 함수를 만들고자 한다.
+ * 이때 잘못된 key가 들어오는 것을 방지하고자 한다.
+ */
 function prtKey(params, key) {
     return params[key];
+}
+prtKey({ name: "zz", phone: "123123123" }, "name");
+// prtKey<ExampleKey, keyof ExampleKey>({name:"zz", phone:"123123123"}, "a"); // error
+/**
+ * class에 제네릭
+ */
+class Clazz {
+    constructor(_data) {
+        this._data = _data;
+    }
+    set data(v) {
+        this._data = v;
+    }
+    get data() {
+        return this._data;
+    }
 }
--- intermediate-ts/keyof.js
+++ intermediate-ts/keyof.js
@@ -1,6 +1,12 @@
 "use strict";
 // 정해진 key만 들어오도록 하고 싶다면 keyof
 // Book에 있는 key로들만 제한을 한다.
+// keyof 타입을 string으로 주면 에러가 나는데 params의 프로퍼티 키인지 타입스크립트가 알 수 없어서 에러가 뜬다.
+/**
+ * keyof 연산자는 객체 타입의 프로퍼티 키를 문자열 리터럴 유니온 타입으로 가져오는 연산자이다.
+ * 따라서 keyof 연산자를 이용하여 반환된 값은 문자열 리터럴 유니온 타입(string | number | ...) 이 되는데
+ * key의 타입을 string으로 주면, params 객체에서 해당 문자열 키가 있는지 검사할 수 없기 때문에 컴파일러가 에러를 발생
+ */
 function print(params, key) {
     console.log(params[key]);
 }
